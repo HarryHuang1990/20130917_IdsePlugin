7ff635cad0a4f74db0eaf1de0b158b55517ebaf0
HarryHuang1990 <Huangfuyang900204@gmail.com>
2013-09-19 09:32:43 +0800

    init

---
+++ src/cn/iscas/idse/format/FileExtractor.java
@@ -0,0 +1,18 @@
+package cn.iscas.idse.format;
+
+/**
+ * An interface for file extracting, which is used to extract text from various formats of file.
+ * The implementation of each format can be constructed by the class FileExtractorFactory
+ */
+public interface FileExtractor {
+	/**
+	 * get the content of text from the file of specific format.
+	 * @return
+	 */
+	public String getContent();
+	/**
+	 * obtain the content of text from the file of specific format.
+	 * @return
+	 */
+	public String getMetaData();
+}
---
+++ src/cn/iscas/idse/format/XmlFileExtractor.java
@@ -0,0 +1,18 @@
+package cn.iscas.idse.format;
+
+public class XmlFileExtractor implements FileExtractor {
+
+	@Override
+	public String getContent() {
+		// TODO Auto-generated method stub
+		System.out.println("xml plugin success");
+		return null;
+	}
+
+	@Override
+	public String getMetaData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
